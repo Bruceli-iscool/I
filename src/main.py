0588b4be07d04a6ad0b0f354973334de1d1ae337
Bruce Li <isanoob1897@outlook.com>
2024-03-01 20:27:07 -0800

    add

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-
 from lexer import Lexer
+from parse import Parse
 
 text_input = """
 print(4 + 4 - 2);
@@ -8,5 +8,9 @@ print(4 + 4 - 2);
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
 
-for token in tokens:
-    print(token)
+pg = Parse()
+pg.parse()
+parser = pg.get_parse()
+parser.parse(tokens).eval()
+
+
--- src/parse.py
+++ src/parse.py
@@ -1,24 +1,44 @@
 from rply import ParserGenerator
 from Abstact_syntax_tree import *
 
-
 class Parse():
     """Parser for i"""
     def __init__(self) -> None:
         self.pg = ParserGenerator(
             ['NUMBER', 'PRINT', 'OPEN_PAREN', 'CLOSED_PAREN', 'SEMI_COLON',
-             'COMMENTS', 'SUM', 'SUB', 'MUL']
-            )
+             'COMMENTS', 'ADD', 'SUB', 'MUL']
+        )
     
     def parse(self):
         @self.pg.production('output : PRINT OPEN_PAREN expression CLOSE_PAREN SEMICOLON')
         def output(p):
             return Print[p[2]]
         
-        @self.pg.production('expression : expression SUM expression')
+        @self.pg.production('expression : expression ADD expression')
         @self.pg.production('expression : expression SUB expression')
         @self.pg.production('expression : expression MUL expression')
         def expression(p):
             left = p[0]
             right = p[2]
             operator = p[1]
+            if operator.gettokentype() == 'ADD':
+                return Add(left, right)
+            elif operator.gettokentype() == 'SUB':
+                return Sub(left, right)
+            elif operator.gettokentype() == 'MUL':
+                return Mul(left, right)
+        
+        @self.pg.production('expression : NUMBER')
+        def number(p):
+            return Number(p[0].value)
+        
+        @self.pg.production('comment : COMMENTS comment')
+        def comment(p):
+            return Comment()
+        
+        @self.pg.error
+        def error_handle(token):
+            raise ValueError(token)
+    
+    def get_parse(self):
+        return self.pg.build()
